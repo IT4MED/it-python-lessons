575d3664970dce0debc182f955ac144a02e01df1
IT4MED_Hub <140026184+IT4MED@users.noreply.github.com>
2023-10-07 13:16:54 +0300

    Update main.py

--- BMI_Calc_1/main.py
+++ BMI_Calc_1/main.py
@@ -1,7 +1,11 @@
-def calculate_bmi(weight, height):
+def calculate_bmi(weight, height_cm):
     try:
         weight = float(weight)
-        height = float(height)
+        height = float(height_cm) / 100
+
+        if height == 0:
+            raise ZeroDivisionError
+
         bmi = weight / (height ** 2)
         return bmi
     except ValueError:
@@ -9,16 +13,24 @@ def calculate_bmi(weight, height):
     except ZeroDivisionError:
         return None
 
+
 def main():
-    weight = input("Введите вес (кг): ")
-    height = input("Введите рост (м): ")
+    try:
+        weight = float(input("Введите вес (кг): "))
+        height_cm = float(input("Введите рост (см): "))
 
-    bmi = calculate_bmi(weight, height)
+        if weight <= 0 or height_cm <= 0:
+            raise ValueError
 
-    if bmi is not None:
-        print(f"Индекс массы тела (BMI): {bmi:.2f}")
-    else:
+        bmi = calculate_bmi(weight, height_cm)
+
+        if bmi is not None:
+            print(f"Индекс массы тела (BMI): {bmi:.2f}")
+        else:
+            print("Ошибка: введены некорректные данные!")
+    except ValueError:
         print("Ошибка: введены некорректные данные!")
+
 
 if __name__ == "__main__":
     main()
